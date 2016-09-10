256e6469e05c1e8164b8937f2c42c9fd9f37a256
chris48s <c.mshaw@yahoo.co.uk>
2016-09-10 14:09:47 +0100

    convert to gml scraper

--- scraper.py
+++ scraper.py
@@ -1,8 +1,14 @@
-from geojson_scraper import scrape
+from gml_scraper import scrape
 
 
-stations_url = "http://ilocal.carmarthenshire.gov.uk/mapping/assets?service=WFS&version=1.1.1&request=GetFeature&typeNames=Polling_Stations&outputFormat=json&srsName=EPSG%3A4326"
+stations_url = "http://ilocal.carmarthenshire.gov.uk/mapping/assets?service=WFS&version=1.1.1&request=GetFeature&typeNames=Polling_Stations&srsName=EPSG%3A4326"
+stations_fields = {
+    '{http://www.carmarthenshire.gov.uk/}register': 'register',
+    '{http://www.carmarthenshire.gov.uk/}address': 'address',
+    '{http://www.carmarthenshire.gov.uk/}postcode': 'postcode',
+}
+
 council_id = 'W06000010'
 
 
-scrape(stations_url, council_id, 'utf-8', 'stations')
+scrape(stations_url, council_id, 'stations', stations_fields, 'register', xml_format='wfs/2.0')
